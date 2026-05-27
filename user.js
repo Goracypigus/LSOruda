6adb2b8a822029f968fc4487e1d5f2344407a30b
Goracypigus <goracypigus@gmail.com>
2026-05-27 17:24:31 +0200

    Delete user.js

--- user.js
+++
@@ -1,4 +0,0 @@
-import { auth, db } from './firebase-config.js';
-// this file intentionally left small; app.js handles user panel in SPA flow
-console.log('user.js loaded');
-window.addEventListener('DOMContentLoaded', ()=>{ /* placeholder */ });
